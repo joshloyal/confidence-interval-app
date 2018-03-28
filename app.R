00c013ab0d2b772a252ff2973758fe02450784f5
Joshua Loyal <jloyal25@gmail.com>
2018-03-27 19:18:03 -0500

    changed to proportion example

--- app.R
+++ app.R
@@ -17,8 +17,8 @@ ui <- fluidPage(
       checkboxInput("show_intervals",
         "Show Confidence Intervals",
         FALSE),
-      checkboxInput("show_true_mean",
-        "Show True Mean",
+      checkboxInput("show_true_prop",
+        "Show True Proportion",
         FALSE),
       sliderInput("n_experiments",
         "Number of experiments:",
@@ -28,10 +28,10 @@ ui <- fluidPage(
         value = 10),
       sliderInput("n_samples",
         "Number of samples:",
-        min = 5,
-        max = 50,
-        step = 5,
-        value = 5),
+        min = 200,
+        max = 1000,
+        step = 100,
+        value = 200),
       sliderInput("alpha",
         "Confidence level:",
         min = 5,
@@ -57,29 +57,39 @@ server <- function(input, output) {
     set.seed(123)
 
     show_intervals <- input$show_intervals
-    show_true_mean <- input$show_true_mean
+    show_true_prop <- input$show_true_prop
     n_experiments = input$n_experiments
     n_samples = input$n_samples
     alpha = 1 - (input$alpha / 100)
+    
+    # create a synthetic population 
+    n_population <- 100000
+    population <- rbernoulli(n_population, p = 0.53)
+    true_proportion <- mean(population)
+    
 
-    true_mean <- 121
-    true_sd <- 8
-
-    sample_means <- vector('numeric', n_experiments)
+    sample_props <- vector('numeric', n_experiments)
     upper_cis <- vector('numeric', n_experiments)
     lower_cis <- vector('numeric', n_experiments)
-    contains_true_mean <- 0
+    contains_true_prop <- 0
     for (i in 1:n_experiments) {
-      sample <- rnorm(n_samples, mean = true_mean, sd = true_sd)
-      sample_means[i] <- mean(sample)
-
-      sample_std <- sd(sample)
-      t_value <- qt(alpha/2, df = n_samples - 1, lower.tail = FALSE)
-      margin_of_error <- t_value * (sample_std / sqrt(n_samples))
-      upper_cis[i] <- sample_means[i] + margin_of_error
-      lower_cis[i] <- sample_means[i] - margin_of_error
-      if (true_mean >= lower_cis[i] && true_mean <= upper_cis[i]) {
-        contains_true_mean <- contains_true_mean + (1/n_experiments)
+      # sample from the population with replacement.
+      sample <- sample(population, n_samples, replace = FALSE)
+      
+      # sample proportion 
+      sample_prop <- mean(sample)
+      
+      # margin of error
+      sample_std <- (sqrt(sample_prop * (1 - sample_prop)) / sqrt(n_samples))
+      z_value <- qnorm(alpha/2, lower.tail = FALSE)
+      margin_of_error <- z_value * sample_std
+      
+      # store values 
+      sample_props[i] <- sample_prop
+      upper_cis[i] <- sample_prop + margin_of_error
+      lower_cis[i] <- sample_prop - margin_of_error
+      if (true_proportion >= lower_cis[i] && true_proportion <= upper_cis[i]) {
+        contains_true_prop <- contains_true_prop + (1/n_experiments)
       }
     }
 
@@ -89,28 +99,30 @@ server <- function(input, output) {
     subtitle_text <- sample_text
     if (show_intervals) {
       subtitle_text <- paste0(sample_text, '\n', 'Observed coverage: ',
-                              round(contains_true_mean, 2) * 100, '%')
+                              round(contains_true_prop, 2) * 100, '%')
     } else {
       subtitle_text <- paste0(sample_text, '\n', 'Observed coverage: N/A')
     }
 
     p <- tibble(
-      sample_mean = sample_means,
+      sample_prop = sample_props,
       upper_ci = upper_cis,
       lower_ci = lower_cis) %>%
       mutate(experiment_number = as.factor(row_number())) %>%
-      ggplot(aes(x = experiment_number, y = sample_mean)) +
+      ggplot(aes(x = experiment_number, y = sample_prop)) +
         geom_point(size = 3, color = 'steelblue') +
-        scale_y_continuous(limits = c(95, 145)) +
+        scale_y_continuous(limits = c(0, 1)) +
         coord_flip() +
         ggtitle(title_text, subtitle = subtitle_text) +
+        ylab('Sample Proportion') + 
+        xlab('Experiment Number') + 
         loyalr::theme_pub() +
         theme(axis.text.y = element_blank(),
               axis.ticks.y = element_blank(),
               axis.line.y = element_blank())
 
-    if (show_true_mean) {
-      p <- p + geom_hline(aes(yintercept = true_mean),
+    if (show_true_prop) {
+      p <- p + geom_hline(aes(yintercept = true_proportion),
                           linetype = 'dashed', size = 1)
     }
 
